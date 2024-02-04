cf6421cdd08a99a8f755cc776ce06cd941cd0e5a
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-02-04 21:12:16 +0200

    translates

--- app/[lang]/pups1.5/additional-components/components.ts
+++
@@ -1,9 +0,0 @@
-export const components = [
-    { title: "(Складові ЗС «PUPS» v.1)", pricePerOne: 6291, quantity: 1 },
-    { title: "Порт прикурювача (видалено)", pricePerOne: -70.0, quantity: 1 },
-    {
-        title: "Розгалужувач USB QC 3.0 (додано)",
-        pricePerOne: +230.0,
-        quantity: 1,
-    },
-];
--- app/[lang]/pups1.5/additional-components/page.tsx
+++ app/[lang]/pups1.5/additional-components/page.tsx
@@ -1,43 +1,63 @@
-import { components } from "./components";
+import { dictionaries } from "../../dictionaries";
 import styles from "./styles.module.scss";
 
-const Page = () => {
-    const totalPrice = components.reduce(
+interface PageProps {
+    params: {
+        lang: string;
+    };
+}
+
+const Page = async ({ params }: PageProps) => {
+    const language = params.lang;
+
+    const strings = dictionaries[language as keyof typeof dictionaries];
+
+    const totalPrice = strings.additionalComponents.components.reduce(
         (acc, current) => acc + current.pricePerOne * current.quantity,
         0,
     );
 
     return (
         <>
-            <h1>Компоненти зарядної станції «PUPS» v.1.5</h1>
+            <h1>{strings.additionalComponents.title}</h1>
             <p className={styles.description}>
-                Ціни вказано на момент вересня-жовтня 2023 року. Ціни враховано
-                без плати за доставку. <br />
-                Порт прикурювача було замінено на швидкий розгалужувач порту USB
-                QC 3.0.
+                {strings.additionalComponents.description} <br />
+                {strings.additionalComponents.descriptionAddtional}
             </p>
             <table className={styles.table}>
                 <thead>
                     <tr>
-                        <th className={styles.name}>Назва</th>
-                        <th className={styles.pricePerOne}>Ціна/штуку</th>
-                        <th className={styles.quantity}>Кількість</th>
-                        <th className={styles.priceFull}>Ціна повна</th>
+                        <th className={styles.name}>
+                            {strings.additionalComponents.thName}
+                        </th>
+                        <th className={styles.pricePerOne}>
+                            {strings.additionalComponents.thPricePerOne}
+                        </th>
+                        <th className={styles.quantity}>
+                            {strings.additionalComponents.thQuantity}
+                        </th>
+                        <th className={styles.priceFull}>
+                            {strings.additionalComponents.thPriceFull}
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {components.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.title}</td>
-                            <td className={styles.tdSecond}>
-                                {item.pricePerOne.toFixed(2)}
-                            </td>
-                            <td>{item.quantity}</td>
-                            <td className={styles.tdFourth}>
-                                {(item.pricePerOne * item.quantity).toFixed(2)}
-                            </td>
-                        </tr>
-                    ))}
+                    {strings.additionalComponents.components.map(
+                        (item, index) => (
+                            <tr key={index}>
+                                <td>{item.title}</td>
+                                <td className={styles.tdSecond}>
+                                    {item.pricePerOne.toFixed(2)}
+                                </td>
+                                <td>{item.quantity}</td>
+                                <td className={styles.tdFourth}>
+                                    {(item.pricePerOne * item.quantity).toFixed(
+                                        2,
+                                    )}
+                                </td>
+                            </tr>
+                        ),
+                    )}
                     <tr>
                         <td></td>
                         <td></td>
