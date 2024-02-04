50dc5fd3003886335de5cc6c7bd6029e56cc0e19
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-02-04 20:05:53 +0200

    faq adaptive

--- app/[lang]/pups1/instruction/page.tsx
+++ app/[lang]/pups1/instruction/page.tsx
@@ -18,26 +18,28 @@ const Page = async ({ params }: PageProps) => {
             <h1>{strings.instruction.title}</h1>
             <ul className={styles.list}>
                 {strings.instruction.list.map((listItem, index) => (
-                    <li className={styles.listLi}>
+                    <li className={styles.listLi} key={index}>
                         <h2>{listItem.title}</h2>
                         {listItem.image && (
                             <img src={listItem.image} alt={listItem.title} />
                         )}
                         {listItem.subUl && (
                             <ul className={styles.subUl}>
-                                {listItem.subUl.map((listItemSub, index) => (
-                                    <li key={index}>{listItemSub}</li>
+                                {listItem.subUl.map((listItemSub, iIndex) => (
+                                    <li key={iIndex}>{listItemSub}</li>
                                 ))}
                             </ul>
                         )}
                         {listItem.description && <p>{listItem.description}</p>}
                         {listItem.descriptionAdditional && (
-                            <p>{listItem.descriptionAdditional}</p>
-                        )}
-                        {listItem.descriptionAdditional1 && (
                             <p>
-                                <br />
-                                {listItem.descriptionAdditional1}
+                                {listItem.descriptionAdditional}
+                                {listItem.descriptionAdditional1 && (
+                                    <>
+                                        <br />
+                                        <>{listItem.descriptionAdditional1}</>
+                                    </>
+                                )}
                             </p>
                         )}
                     </li>
