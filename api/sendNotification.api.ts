9cd90eee33efb3d5f828907cd129df36459af4e6
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-02-14 15:02:13 +0200

    sending notification to email

---
+++ api/sendNotification.api.ts
@@ -0,0 +1,41 @@
+import { API_URL } from "./apiUrl";
+import {
+    INotificationPayload,
+    INotificationResponse,
+} from "../interfaces/notification.interface";
+
+export const NotifcationsApi = {
+    async sendNotifcation({
+        name,
+        email,
+        subject,
+        message,
+    }: INotificationPayload): Promise<INotificationResponse> {
+        try {
+            const response = await fetch(`${API_URL}/notifications/create`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    subject,
+                    message,
+                }),
+            });
+
+            if (response.ok) {
+                return await response.json();
+            } else {
+                const responseData = await response.json();
+
+                throw new Error(responseData.message);
+            }
+        } catch (error: any) {
+            console.log(error);
+
+            return { error: error.message ? error.message : "" };
+        }
+    },
+};
--- components/SupportForm/index.tsx
+++ components/SupportForm/index.tsx
@@ -4,6 +4,9 @@ import { ChangeEvent, FormEvent, useState } from "react";
 
 import { SupportFormProps } from "./component.props";
 import styles from "./styles.module.scss";
+import { Loader } from "../Loader";
+import { NotifcationsApi } from "@/api/sendNotification.api";
+import { toast } from "react-toastify";
 
 export const SupportForm = ({ strings }: SupportFormProps) => {
     const [fields, setFields] = useState({
@@ -12,6 +15,7 @@ export const SupportForm = ({ strings }: SupportFormProps) => {
         subject: "",
         message: "",
     });
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const setFieldsHandler = (
         field: "name" | "email" | "subject" | "message",
@@ -28,52 +32,62 @@ export const SupportForm = ({ strings }: SupportFormProps) => {
     const submit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        try {
-        } catch (error) {
-            console.log("error");
+        setIsLoading(true);
+
+        const response = await NotifcationsApi.sendNotifcation(fields);
+
+        if (response.error) {
+            toast.error(response.error);
+        } else {
+            toast.success("Ви успішно надіслали повідомлення!");
         }
+
+        setIsLoading(false);
     };
 
     return (
-        <form className={styles.form} onSubmit={submit}>
-            <label htmlFor="name">
-                <input
-                    name="name"
-                    type="text"
-                    placeholder={strings.support.feeback.yourName}
-                    required
-                    onChange={(e) => setFieldsHandler("name", e)}
-                />
-            </label>
-            <label htmlFor="email">
-                <input
-                    name="email"
-                    type="email"
-                    placeholder={strings.support.feeback.yourEmail}
-                    required
-                    onChange={(e) => setFieldsHandler("email", e)}
-                />
-            </label>
-            <label htmlFor="topic">
-                <input
-                    name="topic"
-                    type="text"
-                    placeholder={strings.support.feeback.topic}
-                    required
-                    onChange={(e) => setFieldsHandler("subject", e)}
-                />
-            </label>
-            <label htmlFor="message">
-                <textarea
-                    name="message"
-                    placeholder={strings.support.feeback.message}
-                    required
-                    onChange={(e) => setFieldsHandler("message", e)}
-                />
-            </label>
-            <button type="submit" className={styles.button}>
-                {strings.support.feeback.submit}
-            </button>
-        </form>
+        <>
+            <form className={styles.form} onSubmit={submit}>
+                <label htmlFor="name">
+                    <input
+                        name="name"
+                        type="text"
+                        placeholder={strings.support.feeback.yourName}
+                        required
+                        onChange={(e) => setFieldsHandler("name", e)}
+                    />
+                </label>
+                <label htmlFor="email">
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder={strings.support.feeback.yourEmail}
+                        required
+                        onChange={(e) => setFieldsHandler("email", e)}
+                    />
+                </label>
+                <label htmlFor="topic">
+                    <input
+                        name="topic"
+                        type="text"
+                        placeholder={strings.support.feeback.topic}
+                        required
+                        onChange={(e) => setFieldsHandler("subject", e)}
+                    />
+                </label>
+                <label htmlFor="message">
+                    <textarea
+                        name="message"
+                        placeholder={strings.support.feeback.message}
+                        required
+                        onChange={(e) => setFieldsHandler("message", e)}
+                    />
+                </label>
+                <button type="submit" className={styles.button}>
+                    {strings.support.feeback.submit}
+                </button>
+            </form>
+            {isLoading && <Loader />}
+        </>
     );
 };
